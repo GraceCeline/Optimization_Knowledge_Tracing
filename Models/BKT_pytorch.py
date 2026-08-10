7610b928907ab11ec3e8602ca4b3e068debf0a3e
Grace Celine <GraceCeline@dhcp-10-68-130-2.dhcp.uni-bielefeld.de>
2026-08-10 19:08:54 +0200

    different dataset

--- Models/BKT_pytorch.py
+++ Models/BKT_pytorch.py
@@ -7,7 +7,7 @@ import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from sklearn.metrics import roc_auc_score, mean_squared_error, mean_absolute_error, accuracy_score
+from sklearn.metrics import roc_auc_score, mean_squared_error, mean_absolute_error
 # Mainly inspired by https://github.com/JonathanSilver/pyKT/blob/main/bkt.py
 
 class BKT(nn.Module):
@@ -233,9 +233,9 @@ class TorchBKT:
         y_true = pred_df['success'].to_numpy()
         y_pred = pred_df['pred'].to_numpy()
         auc = roc_auc_score(y_true, y_pred)
-        y_pred_binary = (y_pred >= 0.5).astype(int)
-        acc = accuracy_score(y_true, y_pred_binary)
-        return auc, acc
+        rmse = sqrt(mean_squared_error(y_true, y_pred))
+        mae = mean_absolute_error(y_true, y_pred)
+        return auc
     
 
     def save(self, model_dir, scenario_id):
